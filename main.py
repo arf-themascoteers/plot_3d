f9ca5693f6b5c0cbd99752545f2920e098650fe6
aarahman <arif@themascoteers.com>
2023-11-29 00:23:16 +1100

    done

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-source = "data/dataset_66_min.csv"
-
 import matplotlib.pyplot as plt
+import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+from matplotlib import cm
 
-x1 = np.linspace(0, 10, 100)
-x2 = np.linspace(0, 10, 100)
-y = np.sin(x1) + np.cos(x2)
-
+source = "data/results.csv"
+df = pd.read_csv(source)
+i = df["i"].tolist()
+j = df["j"].tolist()
+mse = df["mse"].tolist()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection=Axes3D.name)
 
-ax.plot(x1, x2, y, label='3D Curve')
+fig = plt.figure()
 
-ax.set_xlabel('X1')
-ax.set_ylabel('X2')
-ax.set_zlabel('Y')
+norm = plt.Normalize(min(mse), max(mse))
+colors = cm.viridis(norm(mse))
 
-ax.legend()
+
+line = ax.plot_trisurf(i, j, mse, cmap='viridis', norm=norm, linewidth=0.2, alpha=0.4)
+
+
+ax.set_xlabel("i")
+ax.set_ylabel("j")
+ax.set_zlabel("mse")
+
+mappable = cm.ScalarMappable(norm=norm, cmap=cm.viridis)
+mappable.set_array(mse)
+cbar = plt.colorbar(mappable, ax=ax, label="MSE")
+
+#ax.legend()
 
 plt.show()
--- result_gen.py
+++ result_gen.py
@@ -1,12 +1,31 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 
 SMALL = "data/dataset_66_min.csv"
 BIG = "data/dataset.csv"
 SOURCE = SMALL
 
+
+def create_table(X,y):
+    train_x, test_x, train_y, test_y = train_test_split(X,y, test_size=0.1, random_state=2)
+    df = pd.DataFrame(columns=['i', 'j', 'mse'])
+    for i in range(X.shape[1]):
+        for j in range(X.shape[1]):
+            train_x_new = train_x[:,[i,j]]
+            test_x_new = test_x[:,[i,j]]
+            model = get_model()
+            model.fit(train_x_new, train_y)
+            y_pred = model.predict(test_x_new)
+            mse = mean_squared_error(test_y, y_pred)
+            df.loc[len(df)] = [round(i),round(j),round(mse,3)]
+            print(f"Done {i},{j}")
+    return df
 
 def gen_please():
     data = pd.read_csv(SMALL).to_numpy()
@@ -16,8 +35,14 @@ def gen_please():
         data[:, i] = np.squeeze(x_scaled)
     X = data[:,:-1]
     y = data[:,-1]
+    df = create_table(X, y)
+    df.to_csv("data/results.csv", index=False)
 
 
+
+def get_model():
+    #return LinearRegression()
+    return MLPRegressor(hidden_layer_sizes=(10,), random_state=41)
 
 if __name__ == "__main__":
     gen_please()
